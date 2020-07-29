d83cf1e493cd670c8ba25482ad5f5e5cda8d2645
Ritul Singh <ritulsingh00@gmail.com>
2020-07-29 10:42:40 +0530

    Add files via upload

---
+++ Circle_Spirograph.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(7)
+
+for i in range(6):
+    for colours in ['red', 'magenta', 'blue', 'cyan', 'green', 'yellow', 'white']:
+        turtle.color((colours))
+        turtle.circle(100)
+        turtle.left(10)
+turtle.hideturtle()
