298f7debbf330e561566789872f435c3fe01dc0c
Ritul Singh <ritulsingh00@gmail.com>
2020-08-02 11:08:23 +0530

    Add files via upload

---
+++ Guess the number.py
@@ -0,0 +1,15 @@
+import random
+num = random.randint(0, 100) # Generates a random number between 1 and 100
+num_guess = 1
+while(True):    # Keep running the loop until the number is guessed
+    print("Guess the number between 1 to 100")
+    user = int(input())
+    if user > num:
+        print("Lower number please!")
+    elif user < num:
+        print("Higher number please!")
+    else:
+        print("Congratulations you guess the number")
+        print("You guessed it in" , num_guess , "attempts\n")
+        break
+    num_guess += 1
